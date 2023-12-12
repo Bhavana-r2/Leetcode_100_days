b82e8a7abf4cc121810c9035d7641d4f7a20f48f
Bhavana R <bhavanar.0801@gmail.com>
2023-12-12 19:15:02 +0530

    solution

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,3 +1,12 @@
+
+// Given a sorted integer array nums, modify it in-place to remove duplicates and return the number of unique elements.
+// The first k elements of the modified array should contain the unique elements in their original order. 
+//The judge will verify your solution by comparing it to the expected output using assertions. 
+//Example: for nums = [1,1,2], your function should return 2, and nums should become [1,2,_].
+
+
+
+
 class Solution {
     public int removeDuplicates(int[] nums) {
     int n=nums.length;
