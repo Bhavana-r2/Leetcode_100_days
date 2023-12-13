b9c2f87f5d2cce7f0b08c35405665ade48f6206f
Bhavana R <bhavanar.0801@gmail.com>
2023-12-13 13:12:21 +0530

    solution

---
+++ MinSortedArray.java
@@ -0,0 +1,35 @@
+
+// Given a sorted and rotated array of unique elements, find and return the minimum element. The rotation can occur between 1 and n times. Your algorithm must run in O(log n) time.
+
+// Example:
+
+// Input: nums = [3,4,5,1,2]
+// Output: 1
+// Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+
+// Input: nums = [4,5,6,7,0,1,2]
+// Output: 0
+// Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+
+// Input: nums = [11,13,15,17]
+// Output: 11
+// Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
+
+
+class Solution {
+public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+}
